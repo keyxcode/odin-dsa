af874e8b9d9f4c8b14f90bd80a5def2679d0f825
keyxcode <keyxcode@gmail.com>
2023-03-14 01:05:27 +0700

    Finish 09_replicate

--- 09_replicate/replicate.js
+++ 09_replicate/replicate.js
@@ -1,7 +1,10 @@
-function replicate() {
+function replicate(reps, num) {
   // Return an array containing repetitions of the number argument
   // For instance, replicate(3, 5) should return [5,5,5]
   // If the times argument is negative, return an empty array
+
+  if (reps < 1) return [];
+  return [num].concat(replicate(reps - 1, num));
 }
 
 module.exports = replicate;
--- 09_replicate/replicate.test.js
+++ 09_replicate/replicate.test.js
@@ -1,11 +1,11 @@
 const replicate = require("./replicate");
 
 test("replicate(3, 5) to return [5, 5, 5]", () => {
-  expect(replicate(3, 5)).toBe([5, 5, 5]);
+  expect(replicate(3, 5)).toEqual([5, 5, 5]);
 });
 test("replicate(1, 69) to return [69]", () => {
-  expect(replicate(1, 69)).toBe([69]);
+  expect(replicate(1, 69)).toEqual([69]);
 });
 test("replicate(-2, 6) to return []", () => {
-  expect(replicate(3, 5)).toBe([]);
+  expect(replicate(-2, 6)).toEqual([]);
 });
