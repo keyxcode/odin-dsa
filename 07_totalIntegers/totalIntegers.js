492b07ee6d65ef79be091f984b87b9cb9f6d467e
keyxcode <keyxcode@gmail.com>
2023-03-14 16:02:07 +0700

    Solve problem 07 total integers

--- 07_totalIntegers/totalIntegers.js
+++ 07_totalIntegers/totalIntegers.js
@@ -1,5 +1,14 @@
-function totalIntegers() {
+function totalIntegers(list) {
   // Given a multi-dimensional array, return the total number of integers stored inside this array
+  if (list.length === 0) return 0;
+
+  const firstElement = list.shift();
+  if (Number.isInteger(firstElement)) {
+    return 1 + totalIntegers(list);
+  } else if (!Array.isArray(firstElement)) {
+    // here, firstElement could be a string or a float for example
+    return totalIntegers(list);
+  } else return totalIntegers(firstElement) + totalIntegers(list);
 }
 
 module.exports = totalIntegers;
