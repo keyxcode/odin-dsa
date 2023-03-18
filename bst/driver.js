2557e44809bc009204a0afff8477e0e345c0cbc1
keyxcode <keyxcode@gmail.com>
2023-03-19 01:25:37 +0700

    Implement build tree function

--- bst/driver.js
+++ bst/driver.js
@@ -2,6 +2,21 @@ const treeModule = require("./tree");
 const Tree = treeModule.Tree;
 const TreeNode = treeModule.TreeNode;
 
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
 const myArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+//const myArray = [1, 2, 3];
 const myTree = new Tree(myArray);
-console.log(myTree);
+
+// console.log(myTree);
+// console.log(myTree.left);
+// console.log(myTree.right);
+prettyPrint(myTree);
--- bst/tree.js
+++ bst/tree.js
@@ -4,18 +4,39 @@ class Tree {
     myArrayCopy.sort((a, b) => {
       return a - b;
     });
-    this.root = this.buildTree(myArrayCopy);
+
+    const rootNode = this.buildTree(myArrayCopy);
+    return rootNode;
   }
+
   buildTree(myArray) {
-    return myArray;
+    const myArrayCopy = myArray.slice();
+    const myArrayLength = myArrayCopy.length;
+
+    if (myArrayLength === 0) return null;
+    if (myArrayLength === 1) {
+      return new TreeNode(myArrayCopy[0], null, null);
+    }
+
+    const middleIndex = Math.floor(myArrayLength / 2);
+    const middleElement = myArrayCopy[middleIndex];
+
+    const leftHalf = myArrayCopy.slice(0, middleIndex);
+    const rightHalf = myArrayCopy.slice(middleIndex + 1, myArrayLength);
+
+    return new TreeNode(
+      middleElement,
+      this.buildTree(leftHalf),
+      this.buildTree(rightHalf)
+    );
   }
 }
 
 class TreeNode {
-  constructor(value, leftChild, rightChild) {
+  constructor(value, left, right) {
     this.value = value;
-    this.leftChild = leftChild;
-    this.rightChild = rightChild;
+    this.left = left;
+    this.right = right;
   }
 }
 
