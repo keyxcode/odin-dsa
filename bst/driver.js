e4800cb970e5e70fd74de2d6b0cf2d6dc1b00a04
keyxcode <keyxcode@gmail.com>
2023-03-23 01:11:20 +0700

    Update driver program

--- bst/driver.js
+++ bst/driver.js
@@ -1,26 +1,48 @@
 const treeModule = require("./tree");
 const Tree = treeModule.Tree;
-const TreeNode = treeModule.TreeNode;
 
-// const myTree = new Tree([1, 2, 3, 4, 5, 6, 7]);
-// myTree.prettyPrint();
-// myTree.insert(8);
-// myTree.prettyPrint();
-// myTree.insert(9);
-// myTree.prettyPrint();
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  // The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
-const myTree = new Tree([1, 2, 3, 4, 5, 6, 7]);
-myTree.prettyPrint();
-myTree.insert(9);
-myTree.prettyPrint();
-myTree.remove(1);
-myTree.prettyPrint();
-myTree.remove(3);
+function createArrayRandomNums() {
+  const arrayRandomNums = [];
+  // between
+  const numEls = getRandomInt(1, 100);
+  for (let i = 0; i < numEls; i++) {
+    arrayRandomNums.push(getRandomInt(0, 100));
+  }
+  return arrayRandomNums;
+}
+
+// Create and traverse the tree
+const myArray = createArrayRandomNums();
+console.log(myArray);
+const myTree = new Tree(myArray);
 myTree.prettyPrint();
 console.log(myTree.isBalanced());
+console.log(`level order: ${myTree.levelOrderIterative()}`);
+console.log(`inorder: ${myTree.inorderOptimized()}`);
+console.log(`preorder: ${myTree.preorder()}`);
+console.log(`postorder: ${myTree.postorder()}`);
+
+// Unabalance the tree
+myTree.insert(120);
+myTree.prettyPrint();
+myTree.insert(129);
+myTree.prettyPrint();
+myTree.insert(1239);
+myTree.prettyPrint();
+console.log(myTree.isBalanced());
+
+// Rebalance the tree
 myTree.rebalance();
 myTree.prettyPrint();
 console.log(myTree.isBalanced());
-
-// const myTree2 = new Tree([2, 3, 9]);
-// myTree2.prettyPrint();
+console.log(`level order: ${myTree.levelOrderIterative()}`);
+console.log(`inorder: ${myTree.inorderOptimized()}`);
+console.log(`preorder: ${myTree.preorder()}`);
+console.log(`postorder: ${myTree.postorder()}`);
